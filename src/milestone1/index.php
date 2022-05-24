a7e0edbea3db868face5150411238427f69f036a
nehabht <ne.bhatti2@icloud.com>
2022-05-24 15:50:37 +0200

    initial commit

---
+++ src/milestone1/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+require __DIR__ . '/db.php'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+    
+</head>
+<body>
+
+    <header class="d-flex justify-content-between">
+        <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png" alt="">
+    
+    </header>
+    
+    <main>
+        
+    </main>
+   
+    
+</body>
+</html>
